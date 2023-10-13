5f9a860f225619d18c543da0510a750a52131d12
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-10-13 23:07:33 +0000

    complete the unit test for calc price with bid greater than ask

--- client_test.py
+++ client_test.py
@@ -23,6 +23,12 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+        for quote in quotes:
+            ask_price = float(quote["top_ask"]["price"])
+            bid_price = float(quote["top_bid"]["price"])
+            actual_price = (ask_price + bid_price) / 2
+            _, _, _, price = getDataPoint(quote)
+            self.assertEqual(actual_price, price)
 
 
   """ ------------ Add more unit tests ------------ """
